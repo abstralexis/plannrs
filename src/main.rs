508f9df516928e3b02967ecca5ed139488ca2902
abstralexis <107479436+abstralexis@users.noreply.github.com>
2026-01-07 15:41:01 +0000

    chore: Cargo init and Ratatui Hello World

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use anyhow::Result;
+use ratatui::{DefaultTerminal, Frame, crossterm};
+
+fn main() -> Result<()> {
+    ratatui::run(app)?;
+
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("Hello Ratatui!", frame.area());
+}
